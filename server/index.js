a9570f57a6a7be7ff4894a0f747d3c1d23ed2a82
natEngelking <natalie.mixdorf@gmail.com>
2020-10-07 19:25:59 -0600

    dependencies

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const { token, location } = require('../config.js');
+const cors = require ('cors')
+const bodyParser = require('body-parser');
+const request = require('request-promise')
+const db = require('../db/db.js');
+// const { filter } = require('bluebird');
+const port = 3004;
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '2MB'}));
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+})
